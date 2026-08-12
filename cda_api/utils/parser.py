e55c1d97d57794e9802fa86e89163da2503f40bc
Default <pierlou;ramade@protonmail.com>
2026-08-12 08:47:05 +0000

    fix: stronger datetime parser

--- cda_api/utils/parser.py
+++ cda_api/utils/parser.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
+import logging
+import re
 
 
 class Parser(ABC):
@@ -15,7 +17,21 @@ class Parser(ABC):
 
 
 def parse_time(time_str: str) -> datetime:
-    return datetime.strptime(time_str, "%Y%m%d%H%M%S%z")
+    if re.match(r"\d{14}\+\d{4}", time_str):
+        return datetime.strptime(time_str, "%Y%m%d%H%M%S%z")
+    # *sometimes* dates are badly formatted, trying to recontruct
+    logging.warning(f"{time_str} is not in this expected format, trying to cope")
+    tmp = time_str.split("+")
+    if len(tmp) == 2:
+        d, tz = tmp
+        if len(d) < 14:
+            # guessing seconds are missing
+            d = d.ljust(14, "0")
+        if len(tz) < 4:
+            # too short timezone, adding zeros to the left
+            tz = tz.rjust(4, "0")
+        return datetime.strptime(f"{d}+{tz}", "%Y%m%d%H%M%S%z")
+    raise ValueError(f"Could not parse {time_str} as datetime")
 
 
 def ensure_list(val: list | dict | None) -> list[dict]:
