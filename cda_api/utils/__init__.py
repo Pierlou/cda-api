0b829f998c7b94454d37f360170392cc760a417f
Default <pierlou;ramade@protonmail.com>
2026-09-18 07:45:15 +0000

    feat: first_or_none function

--- cda_api/utils/__init__.py
+++ cda_api/utils/__init__.py
@@ -1,11 +1,12 @@
 from .jsonpath import get
 from .null import NullObject
-from .parser import Parser, ensure_list, last_key, parse_time
+from .parser import Parser, ensure_list, first_or_none, last_key, parse_time
 
 __all__ = [
     "NullObject",
     "Parser",
     "ensure_list",
+    "first_or_none",
     "get",
     "last_key",
     "parse_time",
