95e95fffb796720590d88d00b4e8f06cecf2bcdf
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2026-09-23 17:08:40 +0200

    Refactor API endpoints flow (#9)

--- cda_api/api.py
+++ cda_api/api.py
@@ -1,11 +1,11 @@
 from datetime import date
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 
 from cda_api.utils import first_or_none
 
 if TYPE_CHECKING:
     from cda_api.clinical_doc import ClinicalDocument
-    from cda_api.models.body import Subject, Value
+    from cda_api.models.body import Subject
     from cda_api.models.entity import Entity
 
 
@@ -14,10 +14,53 @@ class Api:
 
     def __init__(self, document: "ClinicalDocument"):
         self.doc = document
+        self._mother_birth_date = None
+        for attr, code, code_type, entry_condition in [
+            ("nb_children_in_household", "85722-7", "qualifier", None),
+            ("child_diet", "67704-7", "qualifier", None),
+            ("mother_gravidity", "11996-6", "code", None),  # nb of pregnancies
+            ("mother_parity", "11977-6", "code", None),  # nb of labours
+            ("mother_premature_babies", "11637-6", "code", None),
+            ("mother_profession", "ORG-099", "qualifier", lambda e: self._is_mother(e.subject)),
+            ("mother_profession", "ORG-099", "qualifier", lambda e: self._is_mother(e.subject)),
+            (
+                "mother_alcohol_during_pregnancy",
+                "74013-4",
+                "code",
+                lambda e: self._is_mother(e.subject),
+            ),
+            (
+                "mother_tobacco_during_pregnancy",
+                "74011-8",
+                "code",
+                lambda e: self._is_mother(e.subject),
+            ),
+            (
+                "mother_tobacco_during_pregnancy",
+                "74011-8",
+                "code",
+                lambda e: self._is_mother(e.subject),
+            ),
+            ("mother_occupation", "ORG-075", "qualifier", lambda e: self._is_mother(e.subject)),
+            ("mother_studies_level", "82589-3", "qualifier", lambda e: self._is_mother(e.subject)),
+            ("father_profession", "ORG-099", "qualifier", lambda e: self._is_father(e.subject)),
+            ("father_occupation", "ORG-075", "qualifier", lambda e: self._is_father(e.subject)),
+            ("father_studies_level", "82589-3", "qualifier", lambda e: self._is_father(e.subject)),
+        ]:
+            setattr(self, attr, self.get_value_from_code(code, code_type, entry_condition))
 
     def iter_entries(self):
         for section in self.doc.component.content:
+            if isinstance(section, str):
+                continue
             yield from section.entries
+
+    def get_value_from_code(self, code: str, code_type: str, entry_condition: Callable | None):
+        for entry in self.iter_entries():
+            if entry_condition is not None and not entry_condition(entry):
+                continue
+            if getattr(entry, code_type) and getattr(entry, f"match_{code_type}")(code):
+                return entry.value.cast()
 
     @property
     def mother(self) -> "Entity | None":
@@ -30,6 +73,10 @@ class Api:
     @property
     def father(self) -> "Entity | None":
         return first_or_none([i for i in self.doc.informant if i.code.code == "FTH"])
+
+    @property
+    def biological_father(self) -> "Entity | None":
+        return first_or_none([i for i in self.doc.informant if i.code.code == "NFTH"])
 
     @staticmethod
     def _is_mother(subj: "Subject | None") -> bool:
@@ -44,87 +91,11 @@ class Api:
         return subj.code.code in {"NFTH", "FTH"}
 
     @property
-    def biological_father(self) -> "Entity | None":
-        return first_or_none([i for i in self.doc.informant if i.code.code == "NFTH"])
-
-    @property
-    def mother_profession(self) -> "Value | None":
-        for entry in self.iter_entries():
-            if self._is_mother(entry.subject) and entry.match_qualifier("ORG-099"):
-                return entry.value
-
-    @property
-    def mother_studies_level(self) -> "Value | None":
-        for entry in self.iter_entries():
-            if self._is_mother(entry.subject) and entry.match_qualifier("82589-3"):
-                return entry.value
-
-    @property
-    def mother_occupation(self) -> "Value | None":
-        for entry in self.iter_entries():
-            if self._is_mother(entry.subject) and entry.match_qualifier("ORG-075"):
-                return entry.value
-
-    @property
-    def father_profession(self) -> "Value | None":
-        for entry in self.iter_entries():
-            if self._is_father(entry.subject) and entry.match_qualifier("ORG-099"):
-                return entry.value
-
-    @property
-    def father_studies_level(self) -> "Value | None":
-        for entry in self.iter_entries():
-            if self._is_father(entry.subject) and entry.match_qualifier("82589-3"):
-                return entry.value
-
-    @property
-    def father_occupation(self) -> "Value | None":
-        for entry in self.iter_entries():
-            if self._is_father(entry.subject) and entry.match_qualifier("ORG-075"):
-                return entry.value
-
-    @property
-    def mother_alcohol_during_pregnancy(self) -> "Value | None":
-        for entry in self.iter_entries():
-            if self._is_mother(entry.subject) and entry.match_code("74013-4"):
-                return entry.value
-
-    @property
-    def mother_tobacco_during_pregnancy(self) -> "Value | None":
-        for entry in self.iter_entries():
-            if self._is_mother(entry.subject) and entry.match_code("74011-8"):
-                return entry.value
-
-    @property
     def mother_birth_date(self) -> date | None:
-        # for whatever reason, the mother's birth date is in both tobbaco and alcohol
-        # consumption entries but not in the informant part
-        for entry in self.iter_entries():
-            if self._is_mother(entry.subject) and entry.code.code in {"74013-4", "74011-8"}:
-                return entry.subject.birth_time
-
-    @property
-    def nb_children_in_household(self) -> "Value | None":
-        for entry in self.iter_entries():
-            if entry.match_qualifier("85722-7"):
-                return entry.value
-
-    @property
-    def child_diet(self) -> "Value | None":
-        for entry in self.iter_entries():
-            if entry.match_qualifier("67704-7"):
-                return entry.value
-
-    @property
-    def mother_gravidity(self) -> "Value | None":
-        # nb of pregnancies
-        for entry in self.iter_entries():
-            if entry.code and entry.match_code("11996-6"):
-                return entry.value
-
-    @property
-    def mother_parity(self) -> "Value | None":
-        # nb of labours
-        for entry in self.iter_entries():
-            if entry.code and entry.match_code("11977-6"):
-                return entry.value
+        if self._mother_birth_date is None:
+            # for whatever reason, the mother's birth date is in both tobbaco and alcohol
+            # consumption entries but not in the informant part
+            for entry in self.iter_entries():
+                if self._is_mother(entry.subject) and entry.code.code in {"74013-4", "74011-8"}:
+                    self._mother_birth_date = entry.subject.birth_time
+        return self._mother_birth_date
